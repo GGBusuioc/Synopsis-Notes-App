9cdf7281aa3aa755e1de63ff52232a0c6529230c
GGBusuioc <ggb.busuioc@gmail.com>
2019-11-16 17:18:15 +0000

    Setting up the React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "notes-ggb-uploads"
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://4dzg6axase.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_pdklmsA9a",
+      APP_CLIENT_ID: "5h2rokj3npvsuite4echkhhdpj",
+      IDENTITY_POOL_ID: "us-east-1:f81f9983-a665-4110-9290-8a66a1a19c24"
+    }
+  };
